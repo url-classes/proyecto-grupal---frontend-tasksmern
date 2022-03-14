d1d70b9d00c947674b93d47bbc3b460a0c173edd
elmergustavo <elmergustavo79@gmail.com>
2022-03-14 11:39:04 -0600

    from para editar perfil

--- src/paginas/EditarPerfil.jsx
+++ src/paginas/EditarPerfil.jsx
@@ -1,72 +1,89 @@
 const EditarPerfil = () => {
   return (
     <>
-      <h2 className="font-black text-3xl text-center mt-2">Editar Perfil</h2>
-      <p className="text-xl mt-5 mb-10 text-center">
-        Modifica tu {""}
-        <span className="text-indigo-600 font-bold">Información aquí</span>
-      </p>
-
-      <div className="flex justify-center">
-        <div className="w-full md:w-1/2 bg-white shadow rounded-lg p-5">
-          
-          <form>
-            <div className="my-3">
-              <label className="uppercase font-bold text-gray-600">
-                Nombre
-              </label>
-              <input
-                type="text"
-                className="border bg-gray-50 w-full p-2 mt-5 rounded-lg"
-                name="nombre"
-                
-                
-              />
-            </div>
-
-            <div className="my-3">
-              <label className="uppercase font-bold text-gray-600">
-                Sitio Web
-              </label>
-              <input
-                type="text"
-                className="border bg-gray-50 w-full p-2 mt-5 rounded-lg"
-                name="web"
-               
-                
-              />
-            </div>
-
-            <div className="my-3">
-              <label className="uppercase font-bold text-gray-600">
-                Teléfono
-              </label>
-              <input
-                type="text"
-                className="border bg-gray-50 w-full p-2 mt-5 rounded-lg"
-                name="telefono"
-                
-              />
-            </div>
-
-            <div className="my-3">
-              <label className="uppercase font-bold text-gray-600">Email</label>
-              <input
-                type="text"
-                className="border bg-gray-50 w-full p-2 mt-5 rounded-lg"
-                name="email"
-                
-              />
-            </div>
-
-            <input
-              type="submit"
-              value="Guardar Cambios"
-              className="bg-indigo-700 px-10 py-3 font-bold text-white rounded-lg uppercase w-full mt-5"
-            />
-          </form>
-        </div>
+      <div>
+        <h1 className="text-4xl text-center md:text-4xl mb-8  ">
+          Editar Perfil
+        </h1>
       </div>
+      <form className="border-4 border-opacity-30 px-3 py-5 md:border-4 border-slate-400 md:px-16 md:py-14 md:mx-28">
+        <div className="grid xl:grid-cols-2 xl:gap-6 mt-9">
+          <div className="relative z-0 mb-6 w-full group">
+            <input
+              type="text"
+              name="nombre"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              required
+            />
+            <label
+              htmlFor="nombre"
+              className="absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Nombre
+            </label>
+          </div>
+          <div className="relative z-0 mb-6 w-full group">
+            <input
+              type="url"
+              name="descripcion"
+              id="descripcion"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              required
+            />
+            <label
+              htmlFor="descripcion"
+              className="absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Sitio web
+            </label>
+          </div>
+        </div>
+
+        <div className="grid xl:grid-cols-2 xl:gap-6 mt-9">
+          <div className="relative z-0 mb-6 w-full group">
+            <input
+              type="tel"
+              name="repeat_password"
+              id="floating_repeat_password"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              required
+            />
+            <label
+              htmlFor="floating_repeat_password"
+              className="absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Telefono
+            </label>
+          </div>
+
+          <div className="relative z-0 mb-6 w-full group">
+            <input
+              type="text"
+              name="repeat_password"
+              id="floating_repeat_password"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              required
+            />
+            <label
+              htmlFor="floating_repeat_password"
+              className="absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Correo Electronico
+            </label>
+          </div>
+        </div>
+
+        <button
+          type="submit"
+          className="text-white bg-indigo-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-20 py-2.5 text-center"
+        >
+          Guardar Cambios
+        </button>
+      </form>
     </>
   );
 };
