a1eec5b367739087733f93ad61b1b9b7325003c4
elmergustavo <elmergustavo79@gmail.com>
2022-04-03 11:48:48 -0600

    solucion de warning con icons

--- src/paginas/Proyecto.jsx
+++ src/paginas/Proyecto.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import useProyectos from "../hooks/useProyectos";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const Proyecto = () => {
   const params = useParams();
@@ -17,17 +18,17 @@ const Proyecto = () => {
 
   console.log(proyecto);
   return cargando ? (
-    <div class="border border-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
-      <div class="animate-pulse flex space-x-4">
-        <div class="rounded-full bg-slate-700 h-10 w-10"></div>
-        <div class="flex-1 space-y-6 py-1">
-          <div class="h-2 bg-slate-700 rounded"></div>
-          <div class="space-y-3">
-            <div class="grid grid-cols-3 gap-4">
-              <div class="h-2 bg-slate-700 rounded col-span-2"></div>
-              <div class="h-2 bg-slate-700 rounded col-span-1"></div>
+    <div className="border border-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
+      <div className="animate-pulse flex space-x-4">
+        <div className="rounded-full bg-slate-700 h-10 w-10"></div>
+        <div className="flex-1 space-y-6 py-1">
+          <div className="h-2 bg-slate-700 rounded"></div>
+          <div className="space-y-3">
+            <div className="grid grid-cols-3 gap-4">
+              <div className="h-2 bg-slate-700 rounded col-span-2"></div>
+              <div className="h-2 bg-slate-700 rounded col-span-1"></div>
             </div>
-            <div class="h-2 bg-slate-700 rounded"></div>
+            <div className="h-2 bg-slate-700 rounded"></div>
           </div>
         </div>
       </div>
@@ -38,25 +39,24 @@ const Proyecto = () => {
       <div className="flex items-center gap-2 text-gray-800 hover:text-black">
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          class="h-6 w-6"
+          className="h-6 w-6"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
-          stroke-width="2"
+          strokeWidth={2}
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
           />
-
         </svg>
 
         <Link
-        to={`/proyectos/editar/${params.id}`}
-        className="uppercase font-bold"
+          to={`/proyectos/editar/${params.id}`}
+          className="uppercase font-bold"
         >
-        Editar
+          Editar
         </Link>
       </div>
     </div>
