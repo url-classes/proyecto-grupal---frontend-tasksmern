f7be58054eebd689b36825de242247423ec463fa
fernando <ferrafterminador@gmail.com>
2022-04-26 00:35:09 -0600

    chat prueba

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -102,12 +102,18 @@ const Sidebar = () => {
         </div>
         {chats.map(chat =>
           <div key={chat._id} className='box'>
-            <h1 className="text-xl">{chat.nombreChat}</h1>
+            {guardarInformacion(chat)}
+            <Link to={`/admin/chat/${chat._id}`}>
+              <h1 className="text-xl">{chat.nombreChat}</h1>
+            </Link>
           </div>)
         }
       </div>
     </aside>
   );
 };
-
+const guardarInformacion = (chat) => {
+  let chatGuardar = JSON.stringify(chat)
+  localStorage.setItem(chat._id, chatGuardar)
+}
 export default Sidebar;
--- src/paginas/ChatPersonal.jsx
+++ src/paginas/ChatPersonal.jsx
@@ -2,24 +2,45 @@ import React from 'react'
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import EditIcon from '@mui/icons-material/Edit';
 import '../css/chatPersonal.css'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { useParams } from 'react-router-dom'
 const ChatPersonal = () => {
+    const params = useParams()
+    let chat = obtenerChat(params.id)
     return (
         <div>
             <div className='flex items-center justify-between md:justify-around'>
                 <ArrowBackIcon fontSize="large" />
                 <h1 className='text-3xl md:text-5xl'>
-                    Chat de alguien
+                    {chat.nombreChat}
                 </h1>
                 <EditIcon fontSize="large" />
             </div>
             <div className='my-3 md:my-5 bg-slate-200 contenido rounded-lg flex flex-col justify-end'>
-                <div>
-
+                <div className='px-2 md:px-4 flex has-tooltip'>
+                    <AccountCircleIcon />{ultimoMensaje(chat.ultimoMensaje)}
+                    <span class='tooltip rounded shadow-lg p-1 bg-gray-100 text-red-500 -mt-8'>{chat.ultimoMensaje.emisor.nombre}</span>
                 </div>
                 <input placeholder='Escribe un mensaje...' className='my-3 md:my-5 py-1 md:py-2 mx-2 md:mx-4 px-2 md:px-4 rounded-lg bg-slate-300 h-12 input-texto'></input>
             </div>
         </div>
     )
+}
+const obtenerChat = (id) => {
+    const chatJSON = localStorage.getItem(id)
+    const chat = JSON.parse(chatJSON)
+    return chat
+}
+const ultimoMensaje = (mensaje) => {
+    if (mensaje != null) {
+        return (
+            <h1>{mensaje.contenido}</h1>
+        )
+    } else {
+        return (
+            <div></div>
+        )
+    }
 }
 
 export default ChatPersonal
